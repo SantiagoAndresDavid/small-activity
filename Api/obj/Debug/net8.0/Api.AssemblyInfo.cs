4f746e407d78c1f76a9c43ec82a79c2b36de669b
SantiagoAndresDavid <sadavid@unicesar.edu.co>
2024-11-18 11:24:03 -0500

    add files

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77fc60a26168c93fd233533793c81c36360b7b18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726841b7bde3f50f46517d42f4d0bab4c787826f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77fc60a26168c93fd233533793c81c36360b7b18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726841b7bde3f50f46517d42f4d0bab4c787826f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
